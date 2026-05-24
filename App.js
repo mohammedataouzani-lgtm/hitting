8b38eedffd2286a047cd90b81186af09eee65a04
lawass1864 <amwass4@hotmail.com>
2026-05-24 20:33:49 +0200

    Ajout de l'écran MesBoxeurs dans le Stack Navigator global d' App.js.
    Nettoyage de BottomTabBar.jsx pour utiliser la navigation standard et correction du nommage de la route.
    
    Résolution d'une double déclaration de la constante TAB_BAR_HEIGHT dans MesBoxeurs.jsx.
    
    Nettoyage complet du cache Metro pour stabiliser l'application.

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import LoginScreen from './app/auth/login.jsx';
 import RegisterScreen from './app/auth/register';
 import DashboardScreen from './app/DashboardScreen';
 import OffresScreen from './app/Paiement';
+// 1. On importe le fameux écran MesBoxeurs
+import MesBoxeursScreen from './app/MesBoxeurs'; 
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +20,10 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        
+        {/* 2. On déclare l'écran ici pour que React Navigation le connaisse enfin */}
+        <Stack.Screen name="MesBoxeurs" component={MesBoxeursScreen} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
